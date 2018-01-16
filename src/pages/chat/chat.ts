b0ab0032d4785554485e87218b6d33fd87a495a4
vitorizkiimanda <vitorizkiimanda@gmail.com>
2018-01-16 17:43:55 +0700

    chats page basic done

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -8,7 +8,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ChatPage {
 
+  chats: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  
+
+    this.chats = [
+      {
+        position: 'left',
+        content: 'Hello from the other side.',
+        senderName: 'Gregory',
+        time: '28-Jun-2016 21:53'
+      },
+      {
+        position: 'right',
+        content: 'Hi! How are?',
+        senderName: 'Me',
+        time: '28-Jun-2016 21:55'
+      },
+      {
+        position: 'left',
+        content: "This is some really long test that I'm writing here. Let's see how it wraps.",
+        senderName: 'Gregory',
+        time: '28-Jun-2016 21:57'
+      }
+    ];
+  
+  
   }
 
   ionViewDidLoad() {
