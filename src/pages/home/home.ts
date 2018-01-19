e2e5e53ab6e712c1ec8bb10769bd6d9df28f303e
vitorizkiimanda <vitorizkiimanda@gmail.com>
2018-01-19 15:50:44 +0700

    home page api basic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-
+import { AuthHttp } from 'angular2-jwt';
+import { Http } from '@angular/http';
 import { Data } from '../../providers/data';
 
 @Component({
@@ -10,16 +11,53 @@ import { Data } from '../../providers/data';
 })
 export class HomePage {
 
+  video:any;
+
+  images:any;
+  slide1:any;
+  slide2:any;
+  slide3:any;
+
   constructor(
     public navCtrl: NavController,
-    public data: Data) {
-      this.data.getData().then((data) => {
-        console.log(data);
-      })
-
-      this.data.getToken().then((data) => {
-        console.log(data);
-      })
+    public data: Data,
+    public authHttp: AuthHttp,
+    public http: Http) {
+      this.getVideo();
+      this.getWallpaper();
   }
 
+  getVideo() {
+    this.authHttp.get(this.data.BASE_URL+"/getvloggers").subscribe(data => {
+      let response = data.json();
+      console.log(response.vloggers);
+      if(response.status==true){
+
+        this.video=response.vloggers;
+      }
+      else{
+        //alert gagal fetch data
+        console.log("error");
+      }
+    });
+  }
+
+  getWallpaper() {
+    this.authHttp.get(this.data.BASE_URL+"/getreviews").subscribe(data => {
+      let response = data.json();
+      console.log(response);
+      if(response.status==true){
+
+        this.images=response.reviews;    
+        this.slide1 = this.images[0].picture;
+        this.slide2 = this.images[1].picture;
+        this.slide3 = this.images[2].picture;
+
+      }
+      else{
+        //alert gagal fetch data
+        console.log("error");
+      }
+    });
+  }
 }
